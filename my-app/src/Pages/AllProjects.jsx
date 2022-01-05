85794b3ea321f59f065762643ddefafa55c4dd6a
cerna234 <cerna2424@gmail.com>
2022-01-04 21:43:13 -0800

    Updated projects Section

--- my-app/src/Pages/AllProjects.jsx
+++ my-app/src/Pages/AllProjects.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import {Project, Projects} from '../Data'
+
 import styled from 'styled-components'
 import Projects from '../Components/Projects'
+
 
 
 const Container = styled.div`
